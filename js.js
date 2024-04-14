106c1d07b5a8779f06c27285debaa3bf56e780cc
Jamie <91485653+LtMax2102@users.noreply.github.com>
2024-04-14 04:35:33 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+let overlay = document.getElementsByClassName("overlay")[0];
+let template = document.querySelector("template").content.querySelector("div");
+let card_con = document.getElementsByClassName("card-container")[0];
+
+overlay.addEventListener("click", (e) => {
+    if (e.target.className == "overlay") {
+        overlay.style.display = "none";
+    }
+})
+
+let info = [
+    ["tilte1", "mainbody1", "cat1.jpg", "white"],
+    ["tilte2", "mainbody2", "cat2.jpg", "blue"],
+    ["tilte3", "mainbody3", "cat3.jpg", "yellow"],
+    ["tilte4", "mainbody4", "cat4.jpg", "salmon"]
+]
+
+window.onload = () => {
+    console.log(info);
+    
+
+    for(let i = 0; i < info.length; i++) {
+        let clone = template.cloneNode(true);
+        clone.querySelector("p").textContent = i;
+        clone.querySelector("img").src = info[i][2];
+        clone.style.background = info[i][3];
+
+        clone.addEventListener("click", (event) => {
+            let id = event.target.parentElement.querySelector("p").textContent;
+
+            overlay.querySelector("header").textContent = info[id][0];
+            overlay.querySelector("main").textContent = info[id][1];
+            overlay.style.display = "flex";
+        });
+        
+        card_con.appendChild(clone);
+        console.log(info[i]);
+    }
+}
